4ae2dd9b8b35fffbae5a46b651d1f2087406a632
Suraj246 <surajsurwase9881@gmail.com>
2023-04-17 12:48:46 +0530

    react-image-slider

---
+++ src/components/ImgData.tsx
@@ -0,0 +1,41 @@
+interface objType {
+    id: number,
+    image: string,
+    title: string,
+    text: string
+}
+
+const ImgData: objType[] = [
+    {
+        id: 0,
+        image: "/Images/slide1.jpg",
+        title: "image1",
+        text: "image1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic doloribus, neque placeat aperiam nisi perferendis eligendi itaque mollitia assumenda dicta, illo beatae vel iure optio consequuntur et atque earum? Beatae.",
+    },
+    {
+        id: 1,
+        image: "/Images/slide2.jpg",
+        title: "image2",
+        text: "inage2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic doloribus, neque placeat aperiam nisi perferendis eligendi itaque mollitia assumenda dicta, illo beatae vel iure optio consequuntur et atque earum? Beatae.",
+    },
+    {
+        id: 2,
+        image: "/Images/slide3.jpg",
+        title: "image3",
+        text: " image3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic doloribus, neque placeat aperiam nisi perferendis eligendi itaque mollitia assumenda dicta, illo beatae vel iure optio consequuntur et atque earum? Beatae.",
+    },
+    {
+        id: 3,
+        image: "/Images/slide4.jpg",
+        title: "image4",
+        text: "image4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic doloribus, neque placeat aperiam nisi perferendis eligendi itaque mollitia assumenda dicta, illo beatae vel iure optio consequuntur et atque earum? Beatae.",
+    },
+    {
+        id: 4,
+        image: "/Images/slide5.jpg",
+        title: "image5",
+        text: "image5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic doloribus, neque placeat aperiam nisi perferendis eligendi itaque mollitia assumenda dicta, illo beatae vel iure optio consequuntur et atque earum? Beatae.",
+    },
+];
+
+export default ImgData;
---
+++ src/components/Slider.tsx
@@ -0,0 +1,123 @@
+import { useEffect, useState, useRef } from "react";
+import ImgData from './ImgData'
+import { Box, Typography } from '@mui/material'
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+function Slider() {
+    const [current, setCurrent] = useState<number>(0);
+    const [intervalTime, setIntervalTime] = useState<string>("start")
+
+    const slideNext = () => {
+        setCurrent(current === ImgData.length - 1 ? 0 : current + 1);
+    };
+    // console.log('current', current);
+    var timer: any;
+    var onChangeRef = useRef(timer)
+    useEffect(() => {
+        if (intervalTime === "start") {
+            onChangeRef.current = setInterval(() => slideNext(), 3000)
+        }
+
+
+        return () => clearInterval(onChangeRef.current)
+    }, [current, intervalTime])
+
+    var stop = () => {
+        clearInterval(onChangeRef.current)
+        setIntervalTime("start")
+
+    }
+
+
+    const slidePrev = () => {
+        setCurrent(current === 0 ? ImgData.length - 1 : current - 1);
+    };
+    const goToIndex = (index: any) => {
+        setCurrent(index)
+    }
+    return (
+        <Box className="slider-page" >
+            <Box className="container-img">
+                {ImgData.map((elem, index) => {
+                    const { image } = elem;
+                    return (
+                        <Box key={index} className="img-slider-container">
+                            {
+                                current ?
+                                    <>
+                                        {index === current &&
+                                            <Box className="flex">
+                                                <Box>
+                                                    <img src={image} alt="" className="img-slide" />
+                                                </Box>
+                                                <Box className="content-container">
+                                                    <Box style={{ display: "grid", gap: "3rem" }}>
+                                                        <Typography className="content" variant="h3" style={{ fontWeight: "500" }}>{elem?.title}</Typography>
+                                                        <Typography className="content">{elem.text}</Typography>
+                                                    </Box>
+                                                </Box>
+                                            </Box>
+                                        }
+                                    </>
+                                    :
+                                    <Box className="flex">
+                                        {index === current &&
+                                            <>
+                                                <Box>
+                                                    <img src={ImgData[0].image} alt="" className="img-slide" />
+                                                </Box>
+                                                <Box className="content-container">
+                                                    <Box style={{ display: "grid", gap: "3rem" }}>
+                                                        <Typography className="content" variant="h3" style={{ fontWeight: "500" }}>{ImgData[0].title}</Typography>
+                                                        <Typography className="content">{ImgData[0].text}</Typography>
+                                                    </Box>
+
+                                                </Box>
+
+                                            </>
+                                        }
+                                    </Box>
+                            }
+
+                        </Box>
+                    )
+                })}
+                <Box className="flex-image">
+
+                    <Box className="arrow-container">
+                        <ArrowCircleLeftIcon onClick={slidePrev} sx={{ fontSize: 50 }} />
+                        <Box className="img-container">
+                            {ImgData.map((elem, index) => {
+                                const { image } = elem;
+                                return (
+                                    <img key={index} src={image} alt=""
+                                        className={current === index ? "" : "img-index"}
+                                        onClick={() => {
+                                            goToIndex(index)
+                                        }}
+                                    />
+                                )
+                            })}
+                        </Box>
+                        <ArrowCircleRightIcon onClick={slideNext} sx={{ fontSize: 50 }} />
+                    </Box>
+                    <Box className="play-container">
+                        {intervalTime === "start" && <PlayCircleIcon sx={{ fontSize: 110 }} style={{ color: "#29b6f6" }}
+                            onClick={() => setIntervalTime("end")}
+                        />}
+                        {intervalTime === "end" &&
+                            <PauseCircleIcon sx={{ fontSize: 110 }} style={{ color: "#29b6f6" }}
+                                onClick={stop}
+                            />
+                        }
+                    </Box>
+                </Box>
+            </Box>
+
+        </Box>
+    );
+}
+
+export default Slider;
